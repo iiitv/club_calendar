1a3ce95dbda9dcc519d70f80b11d4e3c41470157
vidit2512 <201951167@iiitvadodara.ac.in>
2020-09-02 03:03:38 +0530

    add app_test.dart to issue #2

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:club_calendar/main.dart';
 
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:club_calendar/main.dart';
 
 void main() {
   testWidgets('app build smoke test', (WidgetTester tester) async {
