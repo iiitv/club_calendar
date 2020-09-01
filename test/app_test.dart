44677078a7f5b893fe3ad7c1f22ecc61616eda99
vidit2512 <201951167@iiitvadodara.ac.in>
2020-09-02 02:31:30 +0530

    add .travis.yml and app_test.dart to issue #2

---
+++ test/app_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:club_calendar/main.dart';
+
+void main() {
+  testWidgets('app build smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+  });
+}
