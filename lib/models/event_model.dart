143438f3a34c33f3b9eee7ac4c4bdca62ef0fa4e
vikiwarrior <vikrant3302@gmail.com>
2020-09-03 20:54:36 +0530

    data types changed

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -10,33 +10,25 @@ enum Typeofevent {
 
 class EventModel {
   String eventName;
-  String duration;
-  var date;
-  var startTime;
+  int duration;
+  DateTime dateAndTime;
+
   String venue;
   Typeofevent eventType;
-  List speaker;
-  List prizes;
-  List winners;
-  List specialNotes;
+  List<String> speaker;
+  List<String> organisers;
+  Map<String, String> prizes;
+  Map<String, String> winners;
+  String specialNotes;
   EventModel(
       {@required this.eventName,
       @required this.duration,
-      @required this.date,
-      @required this.startTime,
+      @required this.dateAndTime,
       @required this.venue,
       @required this.eventType,
       this.speaker,
+      this.organisers,
       this.prizes,
       this.winners,
       this.specialNotes});
 }
-
-
-//examaple of object declaration
-// void main()
-// {
-// var eventNumber=2;  //this will come from user input
-//    EventModel variableName = EventModel(eventName: " hello World", duration:"2 hrs ",
-//  date: "20:10:2020", startTime:32,venue:"mathclub",  eventType: Typeofevent.values[eventNumber],speaker: ["vikrant" ,"yash"]);
-// }
