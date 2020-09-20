f05e440b247c077f7ef22343fea21773f565e179
Monik09 <monikinderjitsingh5@gmail.com>
2020-09-20 17:32:44 +0530

    Cloud Messaging

--- lib/util/cloud_messaging_handler.dart
+++ lib/util/cloud_messaging_handler.dart
@@ -7,17 +7,14 @@ class CloudMessanger {
       onMessage: (Map<String, dynamic> message) async {
         //if app is already opened just show the notification(app running in foreground)
         print("onMessage: $message");
-        // _showItemDialog(message);
       },
       onLaunch: (Map<String, dynamic> message) async {
         //if app is killed or not opened(not running in background)
         print("onLaunch: $message");
-        // _navigateToItemDetail(message);
       },
       onResume: (Map<String, dynamic> message) async {
         //if app is running in background
         print("onResume: $message");
-        // _navigateToItemDetail(message);
       },
     );
   }
