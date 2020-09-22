cf981ff1c1dcdf075bf21bee1cdff615c8921ef4
Monik09 <monikinderjitsingh5@gmail.com>
2020-09-22 14:18:43 +0530

    Styles File

--- lib/styles.dart
+++ lib/styles.dart
@@ -1,64 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Style {
-	Color getBackgroundColor() {
-		return Color(0xff313131);
-	}
+class Styles {
+  static double elevation = 10.0;
+  static const Color backgroundColor = Color(0xff313131);
+  static const Color buttonColor = Color(0xffF48076);
+  static const Color fontColor = Color(0xffE5E5E5);
+  static const String googleImage = "assets/icons/google_bg.png";
+  static const String logoImage = "assets/icons/CC-Logo(1).png";
 
-	Color getButtonsColor() {
-		return Color(0xffF48076);
-	}
+  Icon getIcon(IconData iconData, {colour= buttonColor,defaultSize=21}) {
+    return Icon(
+      iconData,
+      color: colour,
+      size: defaultSize,
+    );
+  }
 
-	Color getFontColor() {
-		return Color(0xffE5E5E5);
-	}
+  TextStyle titleStyle({color = fontColor}) {
+    return GoogleFonts.montserrat(
+        fontSize: 24, color: color, fontWeight: FontWeight.w500);
+  }
 
-	// Map<String, Color> getLoginScreenBackground() {
-	//   return {
-	//     'bottom': Color(0xff434343),
-	//     'top': Color(0xff575757),
-	//     'background': Color(0xff313131)
-	//   };
-	// }
+  TextStyle headingStyle({color = fontColor}) {
+    return GoogleFonts.montserrat(
+        fontSize: 20, color: color, fontWeight: FontWeight.w500);
+  }
 
-	double elevation() {
-		return 10.0;
-	}
-	
-	static const Color defaultColor = Color(0xffE5E5E5);
-	Color optional;
+  TextStyle descriptionStyle({color = fontColor}) {
+    return GoogleFonts.roboto(
+        fontSize: 18, color: color, fontWeight: FontWeight.w400);
+  }
 
-	TextStyle titleStyle({optional= defaultColor}) {
-		return GoogleFonts.montserrat(
-				fontSize: 24, color: optional, fontWeight: FontWeight.w500);
-	}
-
-	TextStyle headingStyle({optional= defaultColor}) {
-		return GoogleFonts.montserrat(
-				fontSize: 20, color: optional, fontWeight: FontWeight.w500);
-	}
-
-	TextStyle descriptionStyle({optional= defaultColor}) {
-		return GoogleFonts.roboto(
-				fontSize: 18, color: optional, fontWeight: FontWeight.w400);
-	}
-
-
-	List<BoxShadow> shadow() {
-		return [
-			BoxShadow(
-				color: Color(0xff585858),
-				blurRadius: 12,
-				offset: Offset(-4, -4),
-			),
-			BoxShadow(
-				color: Color(0xff000000),
-				blurRadius: 4,
-				offset: Offset(0, 4),
-			),
-		];
-	}
-
-
+  List<BoxShadow> shadow() {
+    return [
+      BoxShadow(
+        color: Color(0xff585858),
+        blurRadius: 12,
+        offset: Offset(-4, -4),
+      ),
+      BoxShadow(
+        color: Color(0xff000000),
+        blurRadius: 4,
+        offset: Offset(0, 4),
+      ),
+    ];
+  }
 }
